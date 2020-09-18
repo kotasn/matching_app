b6f450187b55eed87043095161ea6056b23b06ac
kotasn <kouta4750g@gmail.com>
2020-09-18 19:22:01 +0900

    [add]add to toast

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,7 @@ class RelationshipsController < ApplicationController
   def create
     following = current_user.follow(@user)
     if following.save
-      flash[:success] = 'ユーザーをフォローしました'
+      flash[:success] = 'フォローしました'
       redirect_to user_index_url
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
@@ -15,7 +15,7 @@ class RelationshipsController < ApplicationController
   def destroy
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
+      flash[:warn] = 'フォローを解除しました'
       redirect_to user_index_url
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
